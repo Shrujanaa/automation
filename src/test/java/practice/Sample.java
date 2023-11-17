8c85cb8a3dc669f76d12f36b2b9ec13ad14c6850
Shrujanaa <150109259+Shrujanaa@users.noreply.github.com>
2023-11-17 11:37:38 +0530

    Update Sample.java

--- src/test/java/practice/Sample.java
+++ src/test/java/practice/Sample.java
@@ -15,5 +15,7 @@ public static void main(String[] args) {
 	//print the text of 1st mobile
 	String text = driver.findElement(By.xpath("//span[text()='5G Unlocked Cell Phones with Headphones & Capacitive Pen, Face ID Android 13 Phone, 6.6\" HD+ Display 90HzD 24+64 MP 5500mAh Massive Battery Smartphone 4G+128GB Dual Sim TF Card Purple']")).getText();
 	System.out.println(text);
+
+	System.out.println("I have added new system log into this class.");
 }
 }
